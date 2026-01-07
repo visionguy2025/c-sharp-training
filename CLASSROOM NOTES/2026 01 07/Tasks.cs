d7ab7e8d752fd058f9cadc46282affa2d46256e7
visionguy2025 <vacademy52@gmail.com>
2026-01-07 12:42:26 +0400

    C# first day | January 7, 2026

---
+++ CLASSROOM NOTES/2026 01 07/Tasks.cs
@@ -0,0 +1,85 @@
+#region task1
+// Console.WriteLine("Eded daxil edin:");
+// int limit = int.Parse(Console.ReadLine());
+// for (int i = 1; i <= limit; i++)
+// {
+//     Console.WriteLine(i);
+// };
+#endregion
+
+#region task2
+// string password = "12345";
+// bool isLoop = true;
+// while (isLoop)
+// {
+//     Console.WriteLine("Sifreni daxil edin:");
+//     string input = Console.ReadLine();
+//     if (input == password)
+//     {
+//         Console.WriteLine("Giris ugurludur");
+//         // break;
+//         isLoop = false;
+//     }
+//     else
+//     {
+//         Console.WriteLine("Yanlis sifre, yeniden cehd edin!\n--------------------------");
+//     };
+// };
+#endregion
+
+#region task3
+// int biggestValue;
+// int a = 9;
+// int b = 2;
+// int c = 100;
+// if(a>b)
+// {
+//     biggestValue = a;
+// }
+// else
+// {
+//     biggestValue = b;
+// };
+
+// if(biggestValue > c)
+// {
+//     Console.WriteLine($"En boyuk eded: {biggestValue}");
+// }
+// else
+// {
+//     Console.WriteLine($"En boyuk eded: ${c}");
+// };
+#endregion
+
+#region task4
+// Console.WriteLine("Bali daxil edin:");
+// int inputScore = int.Parse(Console.ReadLine());
+// if (inputScore >= 91)
+// {
+//     Console.WriteLine("Netice: Ela");
+// }
+// else if (inputScore >= 91 && inputScore <= 100)
+// {
+//     Console.WriteLine("Netice: Yaxsi");
+// }
+// else if (inputScore >= 81 && inputScore <=90)
+// {
+//     Console.WriteLine("Netice: Orta");
+// }
+// else if (inputScore >= 71 && inputScore <=80)
+// {
+//     Console.WriteLine("Netice: Pis deyil");
+// }
+// else if (inputScore >= 61 && inputScore <=70)
+// {
+//     Console.WriteLine("Netice: Kafi");
+// }
+// else if (inputScore >= 51 && inputScore <=60)
+// {
+//     Console.WriteLine("...");
+// }
+// else
+// {
+//     Console.WriteLine("--------------------\nNetice: GG");
+// };
+#endregion

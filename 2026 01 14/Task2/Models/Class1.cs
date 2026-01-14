0ec99f910e6217127c72b7d5f5d291c2b79fb627
visionguy2025 <vacademy52@gmail.com>
2026-01-14 12:44:45 +0400

    .

---
+++ 2026 01 14/Task2/Models/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace Models;
+
+public class Validator
+{
+    public string IsValidUsername(string value)
+    {
+        return value;
+    }
+    public string IsValidPassword(string value)
+    {
+        return value;
+    }
+    public string IsValidFirstname(string value)
+    {
+        return value;
+    }
+    public string IsValidLastname(string value)
+    {
+        return value;
+    }
+    public string IsValidBirthdate(string value)
+    {
+        return value;
+    }
+    public int IsValidAge(int value)
+    {
+        return value;
+    }
+};
---
+++ 2026 01 14/Task2/Program/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Models;
+
+public class Person
+{
+    private string username;
+    private string password;
+    private string firstname;
+    private string lastname;
+    private string dateofbirth;
+    private int age;
+
+    public string Username
+    {
+        get
+        {
+            return;
+        }
+        set
+        {
+            username = value;
+        }
+    }
+    public string Password
+    {
+        get
+        {
+            return;
+        }
+        set
+        {
+            password = value;
+        }
+    }
+    public string Firstname
+    {
+        get
+        {
+            return;
+        }
+        set
+        {
+            firstname = value;
+        }
+    }
+    public string Lastname
+    {
+        get
+        {
+            return;
+        }
+        set
+        {
+            lastname = value;
+        }
+    }
+    public string DateOfBirth
+    {
+        get
+        {
+            return;
+        }
+        set
+        {
+            dateofbirth = value;
+        }
+    }
+    public int Age
+    {
+        get
+        {
+            return;
+        }
+        set
+        {
+            age = value;
+        }
+    }
+};
+
+Person NewUser1 = new Person();
+
+NewUser1.Username = "";
+NewUser1.Password = "";
+NewUser1.Firstname = "";
+NewUser1.Lastname = "";
+NewUser1.DateOfBirth = "";
+NewUser1.Age = 0

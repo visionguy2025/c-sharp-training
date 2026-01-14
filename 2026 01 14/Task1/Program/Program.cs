c90371541a3c6e2e966372bda7c2030a14cae28d
visionguy2025 <vacademy52@gmail.com>
2026-01-14 11:51:51 +0400

    Task1 | VA C#

---
+++ 2026 01 14/Task1/Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using classes;
+
+Person user1 = new Person();
+
+// user1.Firstname = "Lorem";
+user1.Firstname = "Lo";
+user1.Lastname = "Ipsum";
+// user1.Age = 30;
+user1.Age = 16;
+user1.BirthDay = "1996-01-14";
+Console.WriteLine(user1.Firstname);
+Console.WriteLine(user1.Lastname);
+Console.WriteLine(user1.Age);
+Console.WriteLine(user1.BirthDay);
+
+// user1.Register("Lorem", "Ipsum", 30, "1996-01-14");
+// user1.ShowDetails();

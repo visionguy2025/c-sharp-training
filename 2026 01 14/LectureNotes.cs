52ebea1c0a875f014a9d5e96f92caa793b15ab4f
visionguy2025 <vacademy52@gmail.com>
2026-01-14 11:53:33 +0400

    Task1

--- 2026 01 14/LectureNotes.cs
+++ 2026 01 14/LectureNotes.cs
@@ -3,13 +3,15 @@
 // internal is default on classes
 // daxilindekilere digerlerini, class a public/internal veririk, nested class class member oldugu ucun istenileni vere bilirik
 // dotnet new classlib
-// property ve field ferqi, property arxa planda oz fieldini yaradir:
+// property ve field ferqi, property arxa planda oz fieldini yaradir: ???
 // public int Age {get; set;} =>
 // public int Age {get{return _age;} set{
 // // conditions ...
 // this._age = value; value => newPersonTypeVariable.Age = "value"
 // }}
 // public int Age {private get; set;}
+// dotnet new classlib
+// dotnet new console
 // dotnet add MyConsoleApp/MyConsoleApp.csproj reference MyLibrary/MyLibrary.csproj | give reference between class lib and console app or any 2 project
 #endregion
 
---
+++ 2026 01 14/Task1/Classes/Class1.cs
@@ -0,0 +1,83 @@
+﻿namespace classes;
+
+public class Person
+{
+    private string firstname;
+    private string lastname;
+    private int age;
+    private string birthday;
+
+    public string Firstname
+    {
+        get
+        {
+            return this.firstname;
+        }
+        set
+        {
+            if(value.Length > 2)
+            {
+                this.firstname = value;
+            }
+            else
+            {
+                Console.WriteLine("Firstname field requires minimum 3 symbols.");
+            };
+        }
+    }
+    public string Lastname
+    {
+        get
+        {
+            return this.lastname;
+        }
+        set
+        {
+            this.lastname = value;
+        }
+    }
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        set
+        {
+            if(value > 16)
+            {
+                this.age = value;
+            }
+            else
+            {
+                Console.WriteLine("Users can't be under 17 years old.");
+            };
+        }
+    }
+    public string BirthDay
+    {
+        get
+        {
+            return this.birthday;
+        }
+        set
+        {
+            this.birthday = value;
+        }
+    }
+
+    // public void ShowDetails()
+    // {
+    //     Console.WriteLine($"Firstname: {this.firstname}");
+    //     Console.WriteLine($"Lastname: {this.lastname}");
+    //     Console.WriteLine($"Age: {this.age}");
+    //     Console.WriteLine($"Birthday: {this.birthday}");
+    // }
+    // public void Register(string fname, string lname, int userage, string birthDay)
+    // {
+    //     this.firstname = fname;
+    //     this.lastname = lname;
+    //     this.age = userage;
+    //     this.birthday = birthDay;
+    // }
+};

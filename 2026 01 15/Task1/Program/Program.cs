edee55197b8f8ef93f702c67f5f3e08331845adf
visionguy2025 <vacademy52@gmail.com>
2026-01-15 12:20:58 +0400

    .

--- 2026 01 15/Task1/Program/Program.cs
+++ 2026 01 15/Task1/Program/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ANIMALS;
+
+internal static class Program
+{
+    internal static void Main(string[] args)
+    {
+        Dog dog1 = new Dog();
+        Cat cat1 = new Cat();
+
+        Animal[] AnimalsArray = {dog1, cat1};
+
+        foreach(Animal animal in AnimalsArray)
+        {
+            animal.Speak();
+        };
+    }
+};

d20452de1f25e9411db85690af1c327a2f7f478c
visionguy2025 <vacademy52@gmail.com>
2026-01-15 11:02:14 +0400

    pt1

---
+++ 2026 01 15/LectureNotes.cs
@@ -0,0 +1,17 @@
+#region RECORD_V_CLASS_TYPE
+// record da initialize olunan zaman deyerler yazilir, sonradan deyismek olmur, class in eksine, istisna
+// MyRecord test = new MyRecord()
+// {
+    // Firstname = "awdawdawd",
+    // Lastname = "aaaaaaa"
+// };
+// MyRecord test1 = test with {Lastname = "awdawdawdawdawdawd"};
+// unvanlar ferqli olacaq (? PROPERTY ISTISANSI OLMASA ??)
+#endregion
+
+#region NAMESPACE
+// eyni adli class lar ferqli namespace ler altinda yaradila biler
+// namespace ler de nested ola biler amma duzgun deyil
+// NameSpace1.ClassName de istifade oluna biler
+// namespace i de deyisene menimsetmek olur, seliqe ucun, cox uzun olarsa istifade etmek olar | using nspc1 = customnamespacehere; npc1.Car car1 = new npc1.Car();
+#endregion

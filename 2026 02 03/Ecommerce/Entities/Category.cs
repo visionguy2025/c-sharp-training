e7acef9cea4ba0e19c4e476adeb1a03c5c52b0f5
visionguy2025 <vacademy52@gmail.com>
2026-02-04 10:45:42 +0400

    .

--- 2026 02 03/Ecommerce/Entities/Category.cs
+++
@@ -1,8 +0,0 @@
-namespace Entity;
-
-public class Category
-{
-    public int Id {get; set;}
-    public string Name {get; set;}
-    public List<Product> Product {get; set;}
-}
---
+++ 2026 02 04/LectureNotes.md
@@ -0,0 +1,2 @@
+Validator (FluentValid..) - DTO
+Mapper (AutoMapper?) - Controller

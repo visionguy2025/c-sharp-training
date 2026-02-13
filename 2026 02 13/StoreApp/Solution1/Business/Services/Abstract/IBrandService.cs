ec96025e5c31744ac516a4760255f177b7f3219d
visionguy2025 <vacademy52@gmail.com>
2026-02-13 09:19:45 +0400

    .

---
+++ 2026 02 13/StoreApp/Solution1/Business/Services/Abstract/IBrandService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.DTOs.Brand;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Services.Abstract
+{
+    public interface IBrandService
+    {
+        public Task<GetBrandDto> GetAllBrands();
+        public Task<GetBrandDto> GetBrandByName();
+        public Task<GetBrandDto> GetBrandById();
+        public Task CreateBrand(CreateBrandDto create);
+        public Task UpdateBrand(Guid id, UpdateBrandDto update);
+    }
+}
---
+++ 2026 02 13/StoreApp/Solution1/Business/Services/Concrete/BrandService.cs
@@ -0,0 +1,48 @@
+﻿using Business.Services.Abstract;
+using Core.Utilities.Exceptions;
+using DataAccessLayer.Repositories.Abstract;
+using Entities.DTOs.Brand;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Services.Concrete
+{
+    public class BrandService : IBrandService
+    {
+        private readonly IBrandRepository _brandRepository;
+
+        public BrandService(IBrandRepository brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+
+        public Task CreateBrand(CreateBrandDto create)
+        {
+            if (await _brandRepository.IsExistsEntity(ex => ex.Name == create.Name))
+            {
+                throw new AlreadyExistException("Eyni adli Brand movcuddur");
+            }
+        }
+
+        public Task<GetBrandDto> GetAllBrands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetBrandDto> GetBrandById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetBrandDto> GetBrandByName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateBrand(Guid id, UpdateBrandDto update)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
